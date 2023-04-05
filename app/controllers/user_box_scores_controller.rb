49f1aa8a6377516a1982f2655b25f1f1452aaa39
Guillaume Cazals <gcazals06@gmail.com>
2023-04-05 13:11:45 +0200

    UserBoxScore index view, wip

--- app/controllers/user_box_scores_controller.rb
+++ app/controllers/user_box_scores_controller.rb
@@ -1,2 +1,8 @@
 class UserBoxScoresController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    club = Club.last
+    @box_scores = UserBoxScore.all
+  end
 end
