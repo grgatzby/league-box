9b63d40e7f17ba9d18eb1d813ab7eb5da47dd67d
Guillaume Cazals <gcazals06@gmail.com>
2023-04-28 13:19:17 +0200

    league table link on lines, improved boxes list show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
   def home
     @club = current_user ? current_user.club : Club.first
   end
+
+  def overview
+    home
+    @manager = User.find_by(is_manager: true)
+  end
 end
