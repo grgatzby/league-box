65bfb3e9d9e6be10c0e5ae6df81c387b14e1fa53
Guillaume Cazals <gcazals06@gmail.com>
2023-11-05 21:36:15 +0100

    improved scrolling up behaviour

--- app/javascript/controllers/contact_us_controller.js
+++ app/javascript/controllers/contact_us_controller.js
@@ -6,30 +6,24 @@ export default class extends Controller {
   static values = { chatroomId: Number }
   static targets = ["message"]
 
-connect(data) {
-    this.channel = createConsumer().subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.#validate(data) }
-      )
-      console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
-    }
+  connect(data) {
+      this.channel = createConsumer().subscriptions.create(
+        { channel: "ChatroomChannel", id: this.chatroomIdValue },
+        { received: data => this.#validate(data) }
+        )
+        console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+      }
 
-submitForm(event) {
-    this.messageTarget.classList.remove("d-none")
-}
+  submitForm(event) {
+      this.messageTarget.classList.remove("d-none")
+  }
 
-
-// resetForm(event) {
-//       event.target.reset()
-//     }
-
-disconnect() {
-  console.log("Unsubscribed from the chatroom")
-  this.channel.unsubscribe()
-}
-#validate() {
-  console.log("message submitted")
-}
-
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
+  #validate() {
+    console.log("message submitted")
+  }
 
 }
