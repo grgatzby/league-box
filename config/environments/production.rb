8ae9e3a0ad0458931387a67c4a0e775856a08810
Guillaume Cazals <gcazals06@gmail.com>
2023-11-07 15:38:16 +0100

    Updating production mailer settings to use MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,10 +4,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "https://league-box.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :port           => ENV['MAIL_SMTP_PORT'],
+    :address        => ENV['MAIL_SMTP_SERVER'],
+    :user_name      => ENV['MAIL_SMTP_LOGIN'],
+    :password       => ENV['MAIL_SMTP_PASSWORD'],
     :domain         => 'league-box.heroku.com',
     :authentication => :plain,
   }
