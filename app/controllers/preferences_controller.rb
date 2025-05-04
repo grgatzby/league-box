eda911c655b56af48532edadd79797dedc9c176b
Guillaume Cazals <gcazals06@gmail.com>
2025-05-04 15:20:51 +0200

    removed cloudinary as active storage and javasciped fading in of front image

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -6,17 +6,15 @@ class PreferencesController < ApplicationController
 
   def create
     # create preference record for current_user
-    #preference = Preference.new(clear_format: params[:clear_format]=="1", user_id: current_user.id)
+    preference = Preference.new(clear_format: params[:clear_format]=="1", user_id: current_user.id)
 
-    preference = Preference.new
-    preference.user_id = current_user.id
-    preference.clear_format = params[:clear_format]=="1"
-    preference.update(photo: preference_params[:photo])
+    #preference.update(photo: preference_params[:photo])
     preference.save
+
     # update current_user details
-    current_user.update(nickname: params[:preference][:nickname],
-    phone_number: params[:preference][:phone_number],
-    email: params[:preference][:e_mail])
+    current_user.update(nickname:     params[:preference][:nickname],
+                        phone_number: params[:preference][:phone_number],
+                        email:        params[:preference][:e_mail])
 
     flash[:notice] = t(".details_stored_flash")
 
@@ -31,7 +29,6 @@ class PreferencesController < ApplicationController
   def update
     # update preference record for current_user, coming form form my_details.html.erb
     preference = Preference.find(params[:id])
-    preference.update(preference_params)
     preference.update(clear_format: params[:clear_format]=="1")
     #image = params[:preference][:photo]
     #preference.photo.attach(io: image,
@@ -39,19 +36,19 @@ class PreferencesController < ApplicationController
     #        content_type: image.content_type)
     #preference.clear_format = params[:clear_format]=="1"
     preference.save
+
     # update current_user details
-    current_user.update(nickname: params[:preference][:nickname],
+    current_user.update(nickname:     params[:preference][:nickname],
                         phone_number: params[:preference][:phone_number],
-                        email: params[:preference][:e_mail])
+                        email:        params[:preference][:e_mail])
 
     flash[:notice] = t(".details_stored_flash")
     redirect_to params[:password] == "1" ? edit_user_registration_path : boxes_path
   end
 
-  private
+#  private
 
-  def preference_params
-    params.require(:preference).permit(:photo, :clear_format)
-    # params.permit!
-  end
+#  def preference_params
+#    params.require(:preference).permit(:photo, :clear_format)
+#  end
 end
--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -13,7 +13,7 @@ const scrollDirection = function() {
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableButton", "togglableElement", "toggler", "topButton"]
+  static targets = ["togglableButton", "togglableElement", "toggler", "topButton", "image"]
   static values = {
     displayRules: String,
     hideRules: String,
@@ -21,19 +21,33 @@ export default class extends Controller {
   }
 
   connect() {
-    var currentElement = this.togglableElementTarget;
-    var oldScrollY = currentElement.scrollTop;
-    var screenType = this.screenTypeValue;
-    var topButton = this.topButtonTarget;
+    var currentElement = this.togglableElementTarget,
+    oldScrollY = currentElement.scrollTop,
+    screenType = this.screenTypeValue,
+    topButton = this.topButtonTarget,
+    image = this.imageTarget,
+    imageH = image.firstElementChild.nextElementSibling.clientHeight
 
     currentElement.onscroll = function(e) {
-      if (oldScrollY < currentElement.scrollTop && currentElement.scrollTop > 0 && screenType !== "mobile") {
+      var hT = image.offsetTop,
+      wH = window.innerHeight,
+      wS = currentElement.scrollTop;
+      //console.log (hT, wH, wS, wS+imageH/2, hT-wH, (wS+imageH/2-(hT-wH))/imageH);
+      if (wS+imageH/2 > (hT-wH)){
+          // console.log('image on the view!');
+          // fade image in at the end of scroll down
+          image.style.opacity = (wS+imageH/2-(hT-wH))/imageH;
+      } else {
+        image.style.opacity = 0;
+      }
+
+      if (oldScrollY < wS && wS > 0 && screenType !== "mobile") {
         // when scrolling UP in the scroll-box, scroll the entire window up if NOT on a mobile screen
         window.scrollTo(0, document.body.scrollHeight);
       }
-      oldScrollY = currentElement.scrollTop;
+      oldScrollY = wS;
       // console.log('top button', topButton.classList);
-      if (currentElement.scrollTop > window.innerHeight/3) {
+      if (wS > wH/3) {
         // when the scrolling through the scroll-box, show the Top button
         topButton.classList.remove("d-none");
       } else {
