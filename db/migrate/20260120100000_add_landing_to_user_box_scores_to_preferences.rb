2da1d3e9927c24b88f74fd2da3c6ab5f5fdf07dd
Guillaume Cazals <gcazals06@gmail.com>
2026-03-06 11:42:18 +0100

    adapt tick box for prod

--- db/migrate/20260120100000_add_landing_to_user_box_scores_to_preferences.rb
+++
@@ -1,5 +0,0 @@
-class AddLandingToUserBoxScoresToPreferences < ActiveRecord::Migration[7.0]
-  def change
-    add_column :preferences, :landing_to_user_box_scores, :boolean, default: false, null: false
-  end
-end
