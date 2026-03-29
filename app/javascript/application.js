7f281fa01bc07b6eb02b2903ee0218f37420c534
Guillaume Cazals <gcazals06@gmail.com>
2026-03-29 23:26:55 +0200

    smooth transition across pages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import "./turbo_page_transitions"
 import "./controllers"
 import "bootstrap"
 import "@popperjs/core"
